357945e759beca98884aea08cd68b729aace2d58
Norman JN Richardson <normanrichardson@gmail.com>
2022-04-13 14:32:02 -0700

    Create apollo server max query batch size

--- src/apolloServer/resolvers/queries/tracks.ts
+++ src/apolloServer/resolvers/queries/tracks.ts
@@ -10,7 +10,7 @@ export default async (parent: any, args: any, context: any, info: any) => {
         ...valueInWhere,
         ...valueExistsWhere,
     ]
-    })).slice(0, parseInt(process.env.QUERY_TRIGGER_BATCH_SIZE!));
+    })).slice(0, parseInt(process.env.SERVE_BATCH_SIZE!));
     
     return tracks;
 };
